694d42d76f128f245161f6a9e8869597e110fd0a
h01m3s <lin426797@gmail.com>
2018-04-27 01:35:21 -0400

    GradientLayer implemented.

---
+++ OdotList/GradientLayer.swift
@@ -0,0 +1,71 @@
+//
+//  GradientLayer.swift
+//  OdotList
+//
+//  Created by Weijie Lin on 4/27/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class GradientLayer: CAGradientLayer {
+    
+    var gradientDirection: GradientDirection? {
+        didSet {
+            guard let gradientDirection = gradientDirection else { return }
+            startPoint = gradientDirection.directionPoint().startPoint
+            endPoint = gradientDirection.directionPoint().endPoint
+        }
+    }
+    
+    init(gradientDirection: GradientDirection = .bottomLeftTopRight) {
+        super.init()
+        startPoint = gradientDirection.directionPoint().startPoint
+        endPoint = gradientDirection.directionPoint().endPoint
+    }
+    
+    override init(layer: Any) {
+        super.init(layer: layer)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+enum GradientDirection {
+    
+    typealias GradientPoint = (startPoint: CGPoint, endPoint: CGPoint)
+    
+    case leftRight
+    case rightLeft
+    case topBottom
+    case bottomTop
+    case topLeftBottomRight
+    case bottomRightTopLeft
+    case topRightBottomLeft
+    case bottomLeftTopRight
+    
+    func directionPoint() -> GradientPoint {
+        switch self {
+        case .leftRight:
+            return (startPoint: CGPoint(x: 0, y: 0.5), endPoint: CGPoint(x: 1, y: 0.5))
+        case .rightLeft:
+            return (startPoint: CGPoint(x: 1, y: 0.5), endPoint: CGPoint(x: 0, y: 0.5))
+        case .topBottom:
+            return (startPoint: CGPoint(x: 0.5, y: 0), endPoint: CGPoint(x: 0.5, y: 1))
+        case .bottomTop:
+            return (startPoint: CGPoint(x: 0.5, y: 1), endPoint: CGPoint(x: 0.5, y: 0))
+        case .topLeftBottomRight:
+            return (startPoint: CGPoint(x: 0, y: 0), endPoint: CGPoint(x: 1, y: 1))
+        case .bottomRightTopLeft:
+            return (startPoint: CGPoint(x: 1, y: 1), endPoint: CGPoint(x: 0, y: 0))
+        case .topRightBottomLeft:
+            return (startPoint: CGPoint(x: 1, y: 0), endPoint: CGPoint(x: 0, y: 1))
+        case .bottomLeftTopRight:
+            return (startPoint: CGPoint(x: 0, y: 1), endPoint: CGPoint(x: 1, y: 0))
+        }
+    }
+    
+}
--- OdotList/ViewController.swift
+++ OdotList/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var first = true
+    
+    let colors = [[UIColor(hexString: "F4736A").cgColor, UIColor(hexString: "F8A05A").cgColor],
+                  [UIColor(hexString: "79D0E2").cgColor, UIColor(hexString: "7494DD").cgColor]]
+    
+    let gradientLayer = GradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        gradientLayer.colors = colors.first
+        gradientLayer.frame = view.frame
+        view.layer.addSublayer(gradientLayer)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap() {
+        let colorChangeAnimation = CABasicAnimation(keyPath: "colors")
+        colorChangeAnimation.duration = 0.5
+        if first == true {
+            colorChangeAnimation.toValue = colors.last
+            first = false
+        } else {
+            colorChangeAnimation.toValue = colors.first
+            first = true
+        }
+        
+        colorChangeAnimation.fillMode = kCAFillModeForwards
+        colorChangeAnimation.isRemovedOnCompletion = false
+        colorChangeAnimation.delegate = self
+        gradientLayer.add(colorChangeAnimation, forKey: "colorChange")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+}
 
+extension ViewController: CAAnimationDelegate {
+
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if flag {
+            print("Done")
+            if first == false {
+                gradientLayer.colors = colors.last
+            } else {
+                gradientLayer.colors = colors.first
+            }
+            view.setNeedsLayout()
+        }
+    }
 
 }
 
