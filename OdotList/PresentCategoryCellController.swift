7cd7f14d338534d505e5df779eaf85173c49647d
h01m3s <lin426797@gmail.com>
2018-05-24 23:37:06 -0400

    PresentCategoryCellController added.

---
+++ OdotList/PresentCategoryCellController.swift
@@ -0,0 +1,24 @@
+//
+//  PresentCategoryCellController.swift
+//  OdotList
+//
+//  Created by Weijie Lin on 5/24/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+class PresentCategoryCellController: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        // Animation
+    }
+    
+    
+    
+    
+}
